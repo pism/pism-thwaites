e8a9a1268c363bd9f85863b23ee824b3b9647922
Constantine Khrulev <ckhroulev@alaska.edu>
2018-02-21 18:40:06 -0900

    FTT, not FFT.

--- preprocess.sh
+++ preprocess.sh
@@ -11,10 +11,10 @@ ncatted -a units,precipitation,o,c,"kg m-2 year-1" albmap_bedmap2_1km_thwaites.n
 gdalwarp -overwrite  -s_srs EPSG:3031 -t_srs EPSG:3031 -cutline basins/thwaites.shp NETCDF:albmap_bedmap2_1km_thwaites.nc:mask thwaites.tif
 # Convert to netCDF
 gdal_translate -of netCDF thwaites.tif thwaites.nc
-# Change variable name to fft_mask
-ncrename -v mask,fft_mask thwaites.nc
+# Change variable name to ftt_mask
+ncrename -v mask,ftt_mask thwaites.nc
 # Add mask to clean input file
-ncks -A -v fft_mask thwaites.nc albmap_bedmap2_1km_thwaites.nc
+ncks -A -v ftt_mask thwaites.nc albmap_bedmap2_1km_thwaites.nc
 
 # Now remove "missing" values
 cdo setrtomiss,1e4,1e38 albmap_bedmap2_1km_thwaites.nc foo.nc
